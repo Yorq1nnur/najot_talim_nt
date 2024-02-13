957bddac062f0e70a1506b00f6d2921d55852897
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-02-13 15:26:25 +0500

    Latest

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:najot_talim_nt/screens/second_task_screen.dart';
+import 'package:najot_talim_nt/screens/task_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
           home: child,
         );
       },
-      child: const SecondTaskScreen(),
+      child: const TaskScreen(),
     );
   }
 }
