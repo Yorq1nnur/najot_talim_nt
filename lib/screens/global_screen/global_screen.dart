5be4f9204f7d55db9fe48902f28113dac52551da
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-03-04 20:47:18 +0500

    BottomNavigationBar

--- lib/screens/global_screen/global_screen.dart
+++ lib/screens/global_screen/global_screen.dart
@@ -2,7 +2,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:najot_talim_nt/utils/colors/app_colors.dart';
+import 'package:najot_talim_nt/utils/styles/app_text_style.dart';
 
 class GlobalScreen extends StatefulWidget {
   const GlobalScreen({super.key});
@@ -16,7 +18,19 @@ class _GlobalScreenState extends State<GlobalScreen> {
   Widget build(BuildContext context) {
     return AnnotatedRegion(
       value: SystemUiOverlayStyle(statusBarColor: AppColors.transparent),
-      child: Scaffold(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "E-COMMERCE",
+            style: AppTextStyle.interBold.copyWith(
+              color: AppColors.black,
+              fontSize: 20.sp,
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+          centerTitle: true,
+        ),
+      ),
     );
   }
 }
--- lib/utils/colors/app_colors.dart
+++ lib/utils/colors/app_colors.dart
@@ -7,10 +7,17 @@ class AppColors {
   static const Color white = Colors.white;
   static const Color transparent = Colors.transparent;
 
-  static const Color c_1A72DD = Color(0xFF1A72DD);
-  static const Color c_5B1CAE = Color(0xFF5B1CAE);
+  static const Color c1A72DD = Color(0xFF1A72DD);
+  static const Color c5B1CAE = Color(0xFF5B1CAE);
   static const Color cF4261A = Color(0xFFF4261A);
-  static const Color c_2A3256 = Color(0xFF2A3256);
-  static const Color cC4C4C4= Color(0xFFC4C4C4);
-  static const Color c0001FC= Color(0xFF0001FC);
+  static const Color c2A3256 = Color(0xFF2A3256);
+  static const Color cC4C4C4 = Color(0xFFC4C4C4);
+  static const Color c0001FC = Color(0xFF0001FC);
+  static const Color c9E5CF2 = Color(0xFF9E5CF2);
+  static const Color c1E2832 = Color(0xFF1E2832);
+  static const Color cFFFFFF = Color(0xFFFFFFFF);
+  static const Color c000000 = Color(0xFF000000);
+  static const Color cE3E3E3 = Color(0xFFE3E3E3);
+  static const Color cF93C00 = Color(0xFFF93C00);
+  static const Color c888888 = Color(0xFF888888);
 }
