47a06a6a919310634e249f4fec73136c4deb430d
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-03-05 16:06:16 +0500

    BottomNavigationBar

--- lib/screens/edit_screen/edit_screen.dart
+++ lib/screens/edit_screen/edit_screen.dart
@@ -5,7 +5,6 @@ import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:najot_talim_nt/data/models/product_model.dart';
 import 'package:najot_talim_nt/screens/edit_screen/items/text_field_item.dart';
-import 'package:najot_talim_nt/screens/global_screen/global_screen.dart';
 import 'package:najot_talim_nt/utils/colors/app_colors.dart';
 import 'package:najot_talim_nt/utils/styles/app_text_style.dart';
 import 'package:zoom_tap_animation/zoom_tap_animation.dart';
