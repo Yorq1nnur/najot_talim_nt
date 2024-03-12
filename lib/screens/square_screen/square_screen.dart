09caac7a0530910d8fe9de9a66264e9c9deb9ae1
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-03-12 23:13:02 +0500

    Animations

--- lib/screens/square_screen/square_screen.dart
+++ lib/screens/square_screen/square_screen.dart
@@ -1,7 +1,5 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
+import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:najot_talim_nt/utils/colors/app_colors.dart';
 import 'package:najot_talim_nt/utils/styles/app_text_style.dart';
@@ -18,21 +16,62 @@ class SquareScreen extends StatefulWidget {
   State<SquareScreen> createState() => _SquareScreenState();
 }
 
-class _SquareScreenState extends State<SquareScreen> {
+class _SquareScreenState extends State<SquareScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    );
+
+    animation =
+        Tween<double>(begin: 0, end: 2 * pi).animate(animationController)
+          ..addListener(() {
+            setState(() {});
+          })
+          ..addStatusListener((status) {
+            if (status == AnimationStatus.completed) {
+              animationController.repeat();
+            } else if (status == AnimationStatus.dismissed) {
+              animationController.repeat();
+            }
+          });
+
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return AnnotatedRegion(
-      value: SystemUiOverlayStyle(
-        statusBarColor: AppColors.transparent,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          widget.title,
+          style: AppTextStyle.interBold.copyWith(
+            color: AppColors.black,
+            fontSize: 20.sp,
+            fontWeight: FontWeight.w900,
+          ),
+        ),
       ),
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            widget.title,
-            style: AppTextStyle.interBold.copyWith(
-              color: AppColors.black,
-              fontSize: 20.sp,
-              fontWeight: FontWeight.w900,
+      body: Center(
+        child: Transform.rotate(
+          angle: animation.value,
+          child: Container(
+            height: 200.h,
+            width: 200.h,
+            decoration: const BoxDecoration(
+              color: Colors.blue,
             ),
           ),
         ),
