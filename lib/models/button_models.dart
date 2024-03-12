ad794ebe3c81bb4a8de2d9362a2f831a643c6ab1
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-03-12 22:21:11 +0500

    Animations

--- lib/models/button_models.dart
+++ lib/models/button_models.dart
@@ -1,28 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:najot_talim_nt/screens/animated_padding_screen/animated_padding_screen.dart';
+import 'package:najot_talim_nt/screens/animation_demo_screen/animation_demo_screen.dart';
+import 'package:najot_talim_nt/screens/circular_screen/circular_screen.dart';
+import 'package:najot_talim_nt/screens/colors_circle_screen/colors_circle_screen.dart';
+import 'package:najot_talim_nt/screens/decorated_box_transition_screen/decorated_box_transition_screen.dart';
+import 'package:najot_talim_nt/screens/flutter_tutorial_kart_screen/flutter_tutorial_kart_screen.dart';
+import 'package:najot_talim_nt/screens/kindacode_screen/kindacode_screen.dart';
+import 'package:najot_talim_nt/screens/loading_screen/loading_screen.dart';
+import 'package:najot_talim_nt/screens/opacity_demo_screen/opacity_demo_screen.dart';
+import 'package:najot_talim_nt/screens/size_screen/size_screen.dart';
+import 'package:najot_talim_nt/screens/square_screen/square_screen.dart';
+import 'package:najot_talim_nt/screens/widgets_screen/widgets_screen.dart';
 
 class ButtonModels {
   final String title;
   final Color color;
+  final Widget screen;
 
   ButtonModels({
     required this.title,
     required this.color,
+    required this.screen,
   });
 }
 
-
 final List<ButtonModels> buttonModels = [
-  ButtonModels(title: "Widgets", color: Colors.red,),
-  ButtonModels(title: "Circular", color: Colors.yellow,),
-  ButtonModels(title: "Square", color: Colors.green,),
-  ButtonModels(title: "Size", color: Colors.brown,),
-  ButtonModels(title: "Loading", color: Colors.black,),
-  ButtonModels(title: "Colors circle", color: Colors.orange,),
-  ButtonModels(title: "Animated Padding + Opacity", color: Colors.blue,),
-  ButtonModels(title: "Animation Demo", color: Colors.pink,),
-  ButtonModels(title: "DecoratedBoxTransition Example", color: Colors.teal,),
-  ButtonModels(title: "Opacity Demo", color: Colors.indigo,),
-  ButtonModels(title: "Woolha.com Flutter Tutorial", color: Colors.amber,),
-  ButtonModels(title: "Kindacode.com", color: Colors.deepOrange,),
-  ButtonModels(title: "Flutter - tutorialkart.com", color: Colors.indigoAccent,),
+  ButtonModels(
+    title: "Widgets",
+    color: Colors.red,
+    screen: const WidgetsScreen(
+      title: 'Widgets',
+    ),
+  ),
+  ButtonModels(
+    title: "Circular",
+    color: Colors.yellow,
+    screen: const CircularScreen(
+      title: 'Circular',
+    ),
+  ),
+  ButtonModels(
+    title: "Square",
+    color: Colors.green,
+    screen: const SquareScreen(
+      title: 'Square',
+    ),
+  ),
+  ButtonModels(
+    title: "Size",
+    color: Colors.brown,
+    screen: const SizeScreen(
+      title: 'Size',
+    ),
+  ),
+  ButtonModels(
+    title: "Loading",
+    color: Colors.indigoAccent,
+    screen: const LoadingScreen(
+      title: 'Loading',
+    ),
+  ),
+  ButtonModels(
+    title: "Colors circle",
+    color: Colors.orange,
+    screen: const ColorsCircleScreen(
+      title: 'Colors circle',
+    ),
+  ),
+  ButtonModels(
+    title: "Animated Padding + Opacity",
+    color: Colors.blue,
+    screen: const AnimatedPaddingScreen(
+      title: 'Animated Padding + Opacity',
+    ),
+  ),
+  ButtonModels(
+    title: "Animation Demo",
+    color: Colors.pink,
+    screen: const AnimatedDemoScreen(
+      title: 'Animation Demo',
+    ),
+  ),
+  ButtonModels(
+    title: "DecoratedBoxTransition Example",
+    color: Colors.teal,
+    screen: const DecoratedBoxTransitionScreen(
+      title: 'DecoratedBoxTransition Example',
+    ),
+  ),
+  ButtonModels(
+    title: "Opacity Demo",
+    color: Colors.indigo,
+    screen: const OpacityDemoScreen(
+      title: 'Opacity Demo',
+    ),
+  ),
+  ButtonModels(
+    title: "Woolha.com Flutter Tutorial",
+    color: Colors.amber,
+    screen: const OpacityDemoScreen(
+      title: 'Woolha.com Flutter Tutorial',
+    ),
+  ),
+  ButtonModels(
+    title: "Kindacode.com",
+    color: Colors.deepOrange,
+    screen: const KindacodeScreen(
+      title: 'Kindacode.com',
+    ),
+  ),
+  ButtonModels(
+    title: "Flutter - tutorialkart.com",
+    color: Colors.indigoAccent,
+    screen: const FlutterTutorialKartScreen(
+      title: 'Flutter - tutorialkart.com',
+    ),
+  ),
 ];
--- lib/screens/global_screen/global_screen.dart
+++ lib/screens/global_screen/global_screen.dart
@@ -3,8 +3,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:najot_talim_nt/models/button_models.dart';
 import 'package:najot_talim_nt/utils/colors/app_colors.dart';
 import 'package:najot_talim_nt/utils/styles/app_text_style.dart';
+import 'package:zoom_tap_animation/zoom_tap_animation.dart';
 
 class GlobalScreen extends StatefulWidget {
   const GlobalScreen({super.key});
@@ -17,10 +19,14 @@ class _GlobalScreenState extends State<GlobalScreen> {
   @override
   Widget build(BuildContext context) {
     return AnnotatedRegion(
-      value: SystemUiOverlayStyle(statusBarColor: AppColors.transparent),
+      value: SystemUiOverlayStyle(
+        statusBarColor: AppColors.transparent,
+      ),
       child: Scaffold(
         appBar: AppBar(
+          backgroundColor: AppColors.white,
           elevation: 0,
+          centerTitle: true,
           title: Text(
             "ANIMATIONS",
             style: AppTextStyle.interBold.copyWith(
@@ -30,11 +36,52 @@ class _GlobalScreenState extends State<GlobalScreen> {
             ),
           ),
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            
-          ],
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ...List.generate(
+                buttonModels.length,
+                (index) => ZoomTapAnimation(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => buttonModels[index].screen,
+                      ),
+                    );
+                  },
+                  child: Container(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 10.h,
+                    ),
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10.h,
+                      horizontal: 20.w,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(
+                        16.r,
+                      ),
+                      color: buttonModels[index].color,
+                    ),
+                    child: Center(
+                      child: Text(
+                        buttonModels[index].title,
+                        style: AppTextStyle.interBold.copyWith(
+                          color: AppColors.black,
+                          fontSize: 16.sp,
+                          fontWeight: FontWeight.w900
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
