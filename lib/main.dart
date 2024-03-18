6f6dec6d1133c376ec05249b7009e00be800d082
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-03-19 02:53:21 +0500

    CALCULATOR

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:najot_talim_nt/repositories/storage_repository/storage_repository.dart';
 import 'package:najot_talim_nt/screens/global_screen/global_screen.dart';
 import 'package:najot_talim_nt/utils/colors/app_colors.dart';
 import 'package:najot_talim_nt/view_models/calculator_view_model.dart';
 import 'package:provider/provider.dart';
 
-void main() async{
-
-  WidgetsFlutterBinding.ensureInitialized();
-  StorageRepository.instance;
-
-  runApp(MultiProvider(
-    providers: [
-      ChangeNotifierProvider(
-        create: (_) => CalculatorViewModel(),
-      ),
-    ],
-    child: const MyApp(),
-  ));
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => CalculatorViewModel(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({
+    super.key,
+  });
 
   @override
-  Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(
-        320,
-        568,
-      ),
-      builder: (context, child) {
-        ScreenUtil.init(context);
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            useMaterial3: false,
-            scaffoldBackgroundColor: AppColors.white,
-          ),
-          home: child,
-        );
-      },
-      child: const GlobalScreen(),
-    );
-  }
+  Widget build(
+    BuildContext context,
+  ) =>
+      ScreenUtilInit(
+        designSize: const Size(
+          320,
+          568,
+        ),
+        builder: (
+          context,
+          child,
+        ) {
+          ScreenUtil.init(
+            context,
+          );
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              useMaterial3: false,
+              scaffoldBackgroundColor: AppColors.white,
+            ),
+            home: child,
+          );
+        },
+        child: const GlobalScreen(),
+      );
 }
