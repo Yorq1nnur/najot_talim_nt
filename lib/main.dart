a6015aeca439286bc0d1a69eab466b98ed181f4a
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-03-19 02:56:28 +0500

    MAIN

--- lib/main.dart
+++ lib/main.dart
@@ -4,31 +4,38 @@ import 'package:najot_talim_nt/screens/global_screen/global_screen.dart';
 import 'package:najot_talim_nt/utils/colors/app_colors.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({
+    super.key,
+  });
 
   @override
-  Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(
-        428,
-        926,
-      ),
-      builder: (context, child) {
-        ScreenUtil.init(context);
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            useMaterial3: false,
-            scaffoldBackgroundColor: AppColors.white,
-          ),
-          home: child,
-        );
-      },
-      child: const GlobalScreen(),
-    );
-  }
+  Widget build(
+    BuildContext context,
+  ) =>
+      ScreenUtilInit(
+        designSize: const Size(
+          428,
+          926,
+        ),
+        builder: (context, child) {
+          ScreenUtil.init(
+            context,
+          );
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              useMaterial3: false,
+              scaffoldBackgroundColor: AppColors.white,
+            ),
+            home: child,
+          );
+        },
+        child: const GlobalScreen(),
+      );
 }
