7ef079ed723292cb1ee97b3492d57e38ebd3fce5
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-03-29 15:25:48 +0500

    LOCATION NOTIFICATIONS

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:najot_talim_nt/screens/global_screen/global_screen.dart';
+import 'package:najot_talim_nt/screens/splash_screen.dart';
 import 'package:najot_talim_nt/utils/colors/app_colors.dart';
+import 'package:najot_talim_nt/view_models/location_view_model.dart';
+import 'package:najot_talim_nt/view_models/maps_view_model.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(
-    const MyApp(),
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_) => LocationViewModel()),
+      ChangeNotifierProvider(create: (_) => MapsViewModel()),
+      ChangeNotifierProvider(create: (_) => LocationViewModel()),
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -30,12 +40,12 @@ class MyApp extends StatelessWidget {
           return MaterialApp(
             debugShowCheckedModeBanner: false,
             theme: ThemeData(
-              useMaterial3: false,
+              useMaterial3: true,
               scaffoldBackgroundColor: AppColors.white,
             ),
             home: child,
           );
         },
-        child: const GlobalScreen(),
+        child: const SplashScreen(),
       );
 }
