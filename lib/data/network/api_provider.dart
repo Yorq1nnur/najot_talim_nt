9c823baa5f6733ecfb21695eb4db59ccd67bff98
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-04-02 01:53:35 +0500

    FETCH DATA FROM NETWORK IN CUBIT

--- lib/data/network/api_provider.dart
+++ lib/data/network/api_provider.dart
@@ -1,25 +1,9 @@
 import 'dart:io';
-
 import '../network_response/network_response.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:najot_talim_nt/data/models/country_model/country_model.dart';
 import '../../utils/constants/app_constants.dart';
-
-class ApiProviderr {
-  static Future<List<CountryModel>> getCountries() async {
-    final response = await http.get(Uri.parse(AppConstants.baseUrl));
-    if (response.statusCode == 200) {
-      final jsonData = json.decode(response.body);
-      List<dynamic> countriesJson = jsonData['data']['countries'];
-      List<CountryModel> countries =
-          countriesJson.map((json) => CountryModel.fromJson(json)).toList();
-      return countries;
-    } else {
-      throw Exception('Failed to load countries');
-    }
-  }
-}
 
 class ApiProvider {
   static Future<NetworkResponse> getCountries() async {
--- lib/screens/global_screen/global_screen.dart
+++ lib/screens/global_screen/global_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:najot_talim_nt/data/models/country_model/country_model.dart';
+import 'package:najot_talim_nt/utils/colors/app_colors.dart';
+import 'package:najot_talim_nt/utils/styles/app_text_style.dart';
 import '../../cubits/currency/countries_cubit.dart';
 import '../../cubits/currency/countries_state.dart';
 import '../../data/models/forms_status.dart';
@@ -10,33 +14,70 @@ class GlobalScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Currencies"),
+    return AnnotatedRegion(
+      value: const SystemUiOverlayStyle(
+        statusBarColor: AppColors.transparent,
       ),
-      body: BlocBuilder<CountriesCubit, CountriesState>(
-        builder: (context, state) {
-          if (state.formsStatus == FormsStatus.loading) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          if (state.formsStatus == FormsStatus.error) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(state.statusText),
-              ],
-            );
-          }
-          return ListView(
-            children: List.generate(state.country.length, (index) {
-              CountryModel countryModel = state.country[index];
-              return ListTile(
-                title: Text(countryModel.name),
-                subtitle: Text(countryModel.capital),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          elevation: 0,
+          title: Text(
+            "COUNTRIES",
+            style: AppTextStyle.interBold.copyWith(
+              color: Colors.black,
+              fontSize: 20.sp,
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+        ),
+        body: BlocBuilder<CountriesCubit, CountriesState>(
+          builder: (context, state) {
+            if (state.formsStatus == FormsStatus.loading) {
+              return const Center(
+                child: CircularProgressIndicator(),
               );
-            }),
-          );
-        },
+            }
+            if (state.formsStatus == FormsStatus.error) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(state.statusText),
+                ],
+              );
+            }
+            return ListView(
+              children: List.generate(
+                state.country.length,
+                (index) {
+                  CountryModel countryModel = state.country[index];
+                  return ListTile(
+                    title: Text(
+                      countryModel.name,
+                      style: AppTextStyle.interBold.copyWith(
+                        color: Colors.black,
+                        fontSize: 20.sp,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                    subtitle: Text(
+                      countryModel.capital,
+                      style: AppTextStyle.interBold.copyWith(
+                        color: Colors.black.withOpacity(
+                          0.5,
+                        ),
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            );
+          },
+        ),
       ),
     );
   }
