e3d0ddfce61125e955f8fd5d340b75a51ff46671
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-04-03 00:57:34 +0500

    ALREADY!!!

--- lib/data/api_provider.dart
+++ lib/data/api_provider.dart
@@ -1,25 +1,3 @@
-// import 'dart:convert';
-// import 'models/network_response.dart';
-// import 'package:http/http.dart' as http;
-// import 'models/trans_actions_model.dart';
-//
-// class ApiProvider {
-//   static Future<NetworkResponse> fetchTransactions() async {
-//     try {
-//       final response = await http.get(Uri.parse("https://banking-api.free.mockoapp.net/transactions-incomes"));
-//       if (response.statusCode == 200) {
-//         final List<dynamic> jsonResponse = json.decode(response.body);
-//         final List<TransactionsModel> transactions = jsonResponse.map((data) => TransactionsModel.fromJson(data)).toList();
-//         return NetworkResponse(data: transactions);
-//       } else {
-//         return NetworkResponse(errorText: 'Failed to load transactions');
-//       }
-//     } catch (e) {
-//       return NetworkResponse(errorText: 'Error: $e');
-//     }
-//   }
-// }
-
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:najot_talim_nt/data/models/trans_actions_model.dart';
@@ -33,9 +11,14 @@ class ApiProvider {
       final response = await http.get(Uri.parse(apiUrl));
       if (response.statusCode == 200) {
         List<dynamic> jsonResponse = json.decode(response.body);
-        List<TransactionsModel> transactions =
-        jsonResponse.map((data) => TransactionsModel.fromJson(data)).toList();
-        return NetworkResponse(data:transactions );
+        List<TransactionsModel> transactions = jsonResponse
+            .map(
+              (data) => TransactionsModel.fromJson(
+                data,
+              ),
+            )
+            .toList();
+        return NetworkResponse(data: transactions);
       } else {
         return NetworkResponse(errorText: "NO'MALUM XATOLIK");
       }
--- lib/screens/currencies/currencies_screen.dart
+++ lib/screens/currencies/currencies_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:najot_talim_nt/utils/styles/app_text_style.dart';
 
 import '../../cubits/currency/currency_cubit.dart';
 import '../../cubits/currency/currency_state.dart';
@@ -31,10 +32,51 @@ class CurrenciesScreen extends StatelessWidget {
             return ListView(
               children: List.generate(state.currencies.length, (index) {
                 TransactionsModel currencyModel = state.currencies[index];
-                return ListTile(
-                  title:
-                  Text("${currencyModel.data[0]} ${currencyModel.data[1]}"),
-                  subtitle: Text("Qiymati: ${currencyModel.transferDate}"),
+                return Container(
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+                  decoration: BoxDecoration(
+                    color: Colors.indigo,
+                    borderRadius: BorderRadius.circular(16),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.indigo.withOpacity(0.4),
+                        spreadRadius: 5
+                      ),BoxShadow(
+                          color: Colors.indigo.withOpacity(0.4),
+                          offset: const Offset(5, 5)
+                      )
+                    ],
+                    border: Border.all(
+                      color: Colors.indigoAccent,
+                      width: 2,
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('YOBORUVCHI NOMI:', style: AppTextStyle.interBold.copyWith(
+                            color: Colors.yellow,
+                            fontSize: 18
+                          ),),
+                          Text('YOBORUVCHI BRENDI:', style: AppTextStyle.interBold.copyWith(
+                              color: Colors.yellow,
+                              fontSize: 18
+                          ),),
+                          Text('YOBORUVCHI JOYLASHUVI:', style: AppTextStyle.interBold.copyWith(
+                              color: Colors.yellow,
+                              fontSize: 18
+                          ),),
+                        ],
+                      )
+                    ],
+                  ),
                 );
               }),
             );
