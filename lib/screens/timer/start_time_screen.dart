c881b875d43154e271fdc333e0fc97c8b2ccb48d
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-04-03 14:45:49 +0500

    ALREADY!!!

--- lib/screens/timer/start_time_screen.dart
+++ lib/screens/timer/start_time_screen.dart
@@ -21,140 +21,135 @@ class _StartTaskScreenState extends State<StartTaskScreen> {
     super.initState();
   }
 
-  bool isCancel = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        leading: IconButton(
-          onPressed: () {
-            isCancel
-                ? Navigator.pop(context)
-                : ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      backgroundColor: Colors.red,
-                      content: Text(
-                        'YOU CANNOT CLOSE THE SCREEN UNTIL THE TASK TIMES OUT!!!',
-                        style: AppTextStyle.interBold.copyWith(
-                          color: Colors.black,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w900,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  );
-          },
-          icon: const Icon(
-            Icons.arrow_back_ios,
-            color: Colors.white,
-            size: 20,
-          ),
-        ),
-      ),
-      body: BlocBuilder<TimeTaskCubit, TimeTaskState>(
-        builder: (BuildContext context, TimeTaskState state) {
-          return SizedBox(
-            width: double.infinity,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  state.taskName,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 5,
-                  ),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Stack(
-                  children: [
-                    SizedBox(
-                      width: 300,
-                      height: 300,
-                      child: CircularProgressIndicator(
-                        value: state.progressValue,
-                        color: Colors.red,
-                        backgroundColor: Colors.blueGrey,
-                        strokeWidth: 8,
-                      ),
-                    ),
-                    Positioned(
-                      right: 30,
-                      bottom: 125,
-                      child: RichText(
-                        text: TextSpan(
-                          text: state.hour.toString().length == 1
-                              ? "0${state.hour.toString()}"
-                              : state.hour.toString(),
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 40,
+    return BlocBuilder<TimeTaskCubit, TimeTaskState>(
+      builder: (BuildContext context, TimeTaskState state) {
+        return Scaffold(
+            backgroundColor: Colors.black,
+            appBar: AppBar(
+              backgroundColor: Colors.black,
+              leading: IconButton(
+                onPressed: () {
+                  state.finishTime
+                      ? Navigator.pop(context)
+                      : ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            backgroundColor: Colors.red,
+                            content: Text(
+                              'YOU CANNOT CLOSE THE SCREEN UNTIL THE TASK TIMES OUT!!!',
+                              style: AppTextStyle.interBold.copyWith(
+                                color: Colors.black,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w900,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
                           ),
-                          children: [
-                            TextSpan(
-                              text: state.minute.toString().length == 1
-                                  ? " : 0${state.minute.toString()} : "
-                                  : " : ${state.minute.toString()} : ",
-                              style: const TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 40,
-                              ),
-                            ),
-                            TextSpan(
-                              text: state.second.toString().length == 1
-                                  ? "0${state.second.toString()}"
-                                  : state.second.toString(),
-                              style: const TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 40,
-                              ),
-                            ),
-                          ],
+                        );
+                },
+                icon: const Icon(
+                  Icons.arrow_back_ios,
+                  color: Colors.white,
+                  size: 20,
+                ),
+              ),
+            ),
+            body: SizedBox(
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    state.taskName,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 5,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Stack(
+                    children: [
+                      SizedBox(
+                        width: 300,
+                        height: 300,
+                        child: CircularProgressIndicator(
+                          value: state.progressValue,
+                          color: Colors.red,
+                          backgroundColor: Colors.blueGrey,
+                          strokeWidth: 8,
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                if (state.finishTime)
-                  ScaffoldMessenger(
-                    child: AlertDialog(
-                      title: const Text('Finish'),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            setState(() {
-                              isCancel = true;
-                            });
-                            context.read<TimeTaskCubit>().finishTaskTime();
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const TimeTaskScreen(),
+                      Positioned(
+                        right: 30,
+                        bottom: 125,
+                        child: RichText(
+                          text: TextSpan(
+                            text: state.hour.toString().length == 1
+                                ? "0${state.hour.toString()}"
+                                : state.hour.toString(),
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 40,
+                            ),
+                            children: [
+                              TextSpan(
+                                text: state.minute.toString().length == 1
+                                    ? " : 0${state.minute.toString()} : "
+                                    : " : ${state.minute.toString()} : ",
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 40,
+                                ),
                               ),
-                            );
-                          },
-                          child: const Text('OK'),
+                              TextSpan(
+                                text: state.second.toString().length == 1
+                                    ? "0${state.second.toString()}"
+                                    : state.second.toString(),
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 40,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ],
-                    ),
-                  )
-              ],
-            ),
-          );
-        },
-      ),
+                      ),
+                    ],
+                  ),
+                  if (state.finishTime)
+                    ScaffoldMessenger(
+                      child: AlertDialog(
+                        title: const Text('Finish'),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              context.read<TimeTaskCubit>().finishTaskTime();
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const TimeTaskScreen(),
+                                ),
+                              );
+                            },
+                            child: const Text('OK'),
+                          ),
+                        ],
+                      ),
+                    )
+                ],
+              ),
+            ));
+      },
     );
   }
 }
