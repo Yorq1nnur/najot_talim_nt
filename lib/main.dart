f5d867481723a786b174ccdac619c63ad7018477
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-04-15 22:02:13 +0500

    1

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,13 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:najot_talim_nt/blocs/books_bloc.dart';
 import 'package:najot_talim_nt/blocs/books_event.dart';
 import 'package:najot_talim_nt/data/repositories/books_repository.dart';
+import 'package:najot_talim_nt/permissions/app_permissions.dart';
 import 'package:najot_talim_nt/screens/global_screen/global_screen.dart';
 import 'package:najot_talim_nt/utils/colors/app_colors.dart';
 import 'data/api_provider/api_provider.dart';
 
-void main() {
+void main() async {
+  await AppPermissions.getNotificationsPermission();
   runApp(
     const App(),
   );
