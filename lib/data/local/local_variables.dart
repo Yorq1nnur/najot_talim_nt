ff9df8ca661d25c4416c8c46e884ef0b968436fb
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-04-24 03:08:36 +0500

    ALREADY!!!

--- lib/data/local/local_variables.dart
+++ lib/data/local/local_variables.dart
@@ -43,5 +43,379 @@ List<BookModel> books = [
     bookName: "Mahbubulqulub",
     bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Mahbubul%20qulub.pdf",
     categoryModel: CategoryModel.comedy,
+  ),BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Sittaizaruriya",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy%20(Foniy).%20Sittai%20zaruriya.pdf",
+    categoryModel: CategoryModel.comedy,
+  ),
+  BookModel(
+    imagePath:
+        "https://cdn1.img.sputniknews.uz/img/07e5/0c/0c/21761440_0:209:2048:2257_1920x0_80_0_0_e6d2cce98730a0ef7b3b4f58241ecf96.jpg",
+    bookName: "G'azallar",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy%20g'azallariga%20sharhlar.pdf",
+    categoryModel: CategoryModel.lesson,
+  ),
+  BookModel(
+    imagePath:
+        "https://proza.ru/pics/2015/10/11/2013.jpg",
+    bookName: "Abrain",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Arba'in.pdf",
+    categoryModel: CategoryModel.action,
+  ),
+  BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Badoibidoya",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Badoyi'%20ul-bidoya.pdf",
+    categoryModel: CategoryModel.lesson,
+  ),
+  BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Hikmatlar",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Hikmatlar.pdf",
+    categoryModel: CategoryModel.action,
+  ),
+  BookModel(
+    imagePath:
+        "https://assets.asaxiy.uz/product/items/desktop/5e15bcce8a4b1.jpg",
+    bookName: "Mahbubulqulub",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Mahbubul%20qulub.pdf",
+    categoryModel: CategoryModel.comedy,
+  ),BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Sittaizaruriya",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy%20(Foniy).%20Sittai%20zaruriya.pdf",
+    categoryModel: CategoryModel.comedy,
+  ),
+  BookModel(
+    imagePath:
+        "https://cdn1.img.sputniknews.uz/img/07e5/0c/0c/21761440_0:209:2048:2257_1920x0_80_0_0_e6d2cce98730a0ef7b3b4f58241ecf96.jpg",
+    bookName: "G'azallar",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy%20g'azallariga%20sharhlar.pdf",
+    categoryModel: CategoryModel.lesson,
+  ),
+  BookModel(
+    imagePath:
+        "https://proza.ru/pics/2015/10/11/2013.jpg",
+    bookName: "Abrain",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Arba'in.pdf",
+    categoryModel: CategoryModel.action,
+  ),
+  BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Badoibidoya",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Badoyi'%20ul-bidoya.pdf",
+    categoryModel: CategoryModel.lesson,
+  ),
+  BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Hikmatlar",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Hikmatlar.pdf",
+    categoryModel: CategoryModel.action,
+  ),
+  BookModel(
+    imagePath:
+        "https://assets.asaxiy.uz/product/items/desktop/5e15bcce8a4b1.jpg",
+    bookName: "Mahbubulqulub",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Mahbubul%20qulub.pdf",
+    categoryModel: CategoryModel.comedy,
+  ),BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Sittaizaruriya",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy%20(Foniy).%20Sittai%20zaruriya.pdf",
+    categoryModel: CategoryModel.comedy,
+  ),
+  BookModel(
+    imagePath:
+        "https://cdn1.img.sputniknews.uz/img/07e5/0c/0c/21761440_0:209:2048:2257_1920x0_80_0_0_e6d2cce98730a0ef7b3b4f58241ecf96.jpg",
+    bookName: "G'azallar",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy%20g'azallariga%20sharhlar.pdf",
+    categoryModel: CategoryModel.lesson,
+  ),
+  BookModel(
+    imagePath:
+        "https://proza.ru/pics/2015/10/11/2013.jpg",
+    bookName: "Abrain",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Arba'in.pdf",
+    categoryModel: CategoryModel.action,
+  ),
+  BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Badoibidoya",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Badoyi'%20ul-bidoya.pdf",
+    categoryModel: CategoryModel.lesson,
+  ),
+  BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Hikmatlar",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Hikmatlar.pdf",
+    categoryModel: CategoryModel.action,
+  ),
+  BookModel(
+    imagePath:
+        "https://assets.asaxiy.uz/product/items/desktop/5e15bcce8a4b1.jpg",
+    bookName: "Mahbubulqulub",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Mahbubul%20qulub.pdf",
+    categoryModel: CategoryModel.comedy,
+  ),BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Sittaizaruriya",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy%20(Foniy).%20Sittai%20zaruriya.pdf",
+    categoryModel: CategoryModel.comedy,
+  ),
+  BookModel(
+    imagePath:
+        "https://cdn1.img.sputniknews.uz/img/07e5/0c/0c/21761440_0:209:2048:2257_1920x0_80_0_0_e6d2cce98730a0ef7b3b4f58241ecf96.jpg",
+    bookName: "G'azallar",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy%20g'azallariga%20sharhlar.pdf",
+    categoryModel: CategoryModel.lesson,
+  ),
+  BookModel(
+    imagePath:
+        "https://proza.ru/pics/2015/10/11/2013.jpg",
+    bookName: "Abrain",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Arba'in.pdf",
+    categoryModel: CategoryModel.action,
+  ),
+  BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Badoibidoya",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Badoyi'%20ul-bidoya.pdf",
+    categoryModel: CategoryModel.lesson,
+  ),
+  BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Hikmatlar",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Hikmatlar.pdf",
+    categoryModel: CategoryModel.action,
+  ),
+  BookModel(
+    imagePath:
+        "https://assets.asaxiy.uz/product/items/desktop/5e15bcce8a4b1.jpg",
+    bookName: "Mahbubulqulub",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Mahbubul%20qulub.pdf",
+    categoryModel: CategoryModel.comedy,
+  ),BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Sittaizaruriya",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy%20(Foniy).%20Sittai%20zaruriya.pdf",
+    categoryModel: CategoryModel.comedy,
+  ),
+  BookModel(
+    imagePath:
+        "https://cdn1.img.sputniknews.uz/img/07e5/0c/0c/21761440_0:209:2048:2257_1920x0_80_0_0_e6d2cce98730a0ef7b3b4f58241ecf96.jpg",
+    bookName: "G'azallar",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy%20g'azallariga%20sharhlar.pdf",
+    categoryModel: CategoryModel.lesson,
+  ),
+  BookModel(
+    imagePath:
+        "https://proza.ru/pics/2015/10/11/2013.jpg",
+    bookName: "Abrain",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Arba'in.pdf",
+    categoryModel: CategoryModel.action,
+  ),
+  BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Badoibidoya",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Badoyi'%20ul-bidoya.pdf",
+    categoryModel: CategoryModel.lesson,
+  ),
+  BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Hikmatlar",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Hikmatlar.pdf",
+    categoryModel: CategoryModel.action,
+  ),
+  BookModel(
+    imagePath:
+        "https://assets.asaxiy.uz/product/items/desktop/5e15bcce8a4b1.jpg",
+    bookName: "Mahbubulqulub",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Mahbubul%20qulub.pdf",
+    categoryModel: CategoryModel.comedy,
+  ),BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Sittaizaruriya",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy%20(Foniy).%20Sittai%20zaruriya.pdf",
+    categoryModel: CategoryModel.comedy,
+  ),
+  BookModel(
+    imagePath:
+        "https://cdn1.img.sputniknews.uz/img/07e5/0c/0c/21761440_0:209:2048:2257_1920x0_80_0_0_e6d2cce98730a0ef7b3b4f58241ecf96.jpg",
+    bookName: "G'azallar",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy%20g'azallariga%20sharhlar.pdf",
+    categoryModel: CategoryModel.lesson,
+  ),
+  BookModel(
+    imagePath:
+        "https://proza.ru/pics/2015/10/11/2013.jpg",
+    bookName: "Abrain",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Arba'in.pdf",
+    categoryModel: CategoryModel.action,
+  ),
+  BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Badoibidoya",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Badoyi'%20ul-bidoya.pdf",
+    categoryModel: CategoryModel.lesson,
+  ),
+  BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Hikmatlar",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Hikmatlar.pdf",
+    categoryModel: CategoryModel.action,
+  ),
+  BookModel(
+    imagePath:
+        "https://assets.asaxiy.uz/product/items/desktop/5e15bcce8a4b1.jpg",
+    bookName: "Mahbubulqulub",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Mahbubul%20qulub.pdf",
+    categoryModel: CategoryModel.comedy,
+  ),BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Sittaizaruriya",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy%20(Foniy).%20Sittai%20zaruriya.pdf",
+    categoryModel: CategoryModel.comedy,
+  ),
+  BookModel(
+    imagePath:
+        "https://cdn1.img.sputniknews.uz/img/07e5/0c/0c/21761440_0:209:2048:2257_1920x0_80_0_0_e6d2cce98730a0ef7b3b4f58241ecf96.jpg",
+    bookName: "G'azallar",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy%20g'azallariga%20sharhlar.pdf",
+    categoryModel: CategoryModel.lesson,
+  ),
+  BookModel(
+    imagePath:
+        "https://proza.ru/pics/2015/10/11/2013.jpg",
+    bookName: "Abrain",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Arba'in.pdf",
+    categoryModel: CategoryModel.action,
+  ),
+  BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Badoibidoya",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Badoyi'%20ul-bidoya.pdf",
+    categoryModel: CategoryModel.lesson,
+  ),
+  BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Hikmatlar",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Hikmatlar.pdf",
+    categoryModel: CategoryModel.action,
+  ),
+  BookModel(
+    imagePath:
+        "https://assets.asaxiy.uz/product/items/desktop/5e15bcce8a4b1.jpg",
+    bookName: "Mahbubulqulub",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Mahbubul%20qulub.pdf",
+    categoryModel: CategoryModel.comedy,
+  ),BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Sittaizaruriya",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy%20(Foniy).%20Sittai%20zaruriya.pdf",
+    categoryModel: CategoryModel.comedy,
+  ),
+  BookModel(
+    imagePath:
+        "https://cdn1.img.sputniknews.uz/img/07e5/0c/0c/21761440_0:209:2048:2257_1920x0_80_0_0_e6d2cce98730a0ef7b3b4f58241ecf96.jpg",
+    bookName: "G'azallar",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy%20g'azallariga%20sharhlar.pdf",
+    categoryModel: CategoryModel.lesson,
+  ),
+  BookModel(
+    imagePath:
+        "https://proza.ru/pics/2015/10/11/2013.jpg",
+    bookName: "Abrain",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Arba'in.pdf",
+    categoryModel: CategoryModel.action,
+  ),
+  BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Badoibidoya",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Badoyi'%20ul-bidoya.pdf",
+    categoryModel: CategoryModel.lesson,
+  ),
+  BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Hikmatlar",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Hikmatlar.pdf",
+    categoryModel: CategoryModel.action,
+  ),
+  BookModel(
+    imagePath:
+        "https://assets.asaxiy.uz/product/items/desktop/5e15bcce8a4b1.jpg",
+    bookName: "Mahbubulqulub",
+    bookUrl: "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Mahbubul%20qulub.pdf",
+    categoryModel: CategoryModel.comedy,
+  ), BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Sittaizaruriya",
+    bookUrl:
+        "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy%20(Foniy).%20Sittai%20zaruriya.pdf",
+    categoryModel: CategoryModel.comedy,
+  ),
+  BookModel(
+    imagePath:
+        "https://cdn1.img.sputniknews.uz/img/07e5/0c/0c/21761440_0:209:2048:2257_1920x0_80_0_0_e6d2cce98730a0ef7b3b4f58241ecf96.jpg",
+    bookName: "G'azallar",
+    bookUrl:
+        "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy%20g'azallariga%20sharhlar.pdf",
+    categoryModel: CategoryModel.lesson,
+  ),
+  BookModel(
+    imagePath: "https://proza.ru/pics/2015/10/11/2013.jpg",
+    bookName: "Abrain",
+    bookUrl:
+        "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Arba'in.pdf",
+    categoryModel: CategoryModel.action,
+  ),
+  BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Badoibidoya",
+    bookUrl:
+        "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Badoyi'%20ul-bidoya.pdf",
+    categoryModel: CategoryModel.lesson,
+  ),
+  BookModel(
+    imagePath:
+        "https://rekhta.pc.cdn.bitgravity.com/Images/EBooks/small_tauzeehat-asbab-e-sitta-zaruriya-abu-waris-jameel-ebooks.jpg",
+    bookName: "Hikmatlar",
+    bookUrl:
+        "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Hikmatlar.pdf",
+    categoryModel: CategoryModel.action,
+  ),
+  BookModel(
+    imagePath:
+        "https://assets.asaxiy.uz/product/items/desktop/5e15bcce8a4b1.jpg",
+    bookName: "Mahbubulqulub",
+    bookUrl:
+        "https://n.ziyouz.com/books/alisher_navoiy_asarlari/Alisher%20Navoiy.%20Mahbubul%20qulub.pdf",
+    categoryModel: CategoryModel.comedy,
   ),
 ];
