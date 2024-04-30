70f1e223dcb3e098a43852d928327e65c1a0a088
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-04-30 16:02:59 +0500

    1

--- lib/screen/currency_screen/home_screen.dart
+++ lib/screen/currency_screen/home_screen.dart
@@ -40,7 +40,6 @@ class CurrencyScreen extends StatelessWidget {
                 return Container(
                   margin: const EdgeInsets.symmetric(
                     vertical: 10,
-                    horizontal: 10,
                   ),
                   padding: const EdgeInsets.symmetric(
                     horizontal: 20,
